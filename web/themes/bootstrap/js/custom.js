8e4c5950571649a139da53dc3bd78d10d573b0b7
ganeshdhadwad <ganesh.dhadwad@iksulaops.com>
2019-09-26 11:06:54 +0530

    initial commit

---
+++ web/themes/bootstrap/js/custom.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * Bootstrap Tooltips.
+ */
+
+var Drupal = Drupal || {};
+
+(function ($) {
+  "use strict";
+
+  // var $id = $("#edit-field-heading-value").css('color','#ff0000');
+  // alert($id);
+  Drupal.behaviors.bootstrapCustom = {
+    attach: function (context) {
+      // Immediately return if tooltips are not available.
+      // alert('WORKING');
+    },
+  };
+
+})(window.jQuery);
